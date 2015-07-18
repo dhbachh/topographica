3a1c9e8a11ed12395808dcee9aa5a21bd5d1df64
Bachir Djelmami-Hani <s1142191@falconara.inf.ed.ac.uk>
2015-07-18 15:35:41 +0100

    fix

--- topo/submodel/earlyvision.py
+++ topo/submodel/earlyvision.py
@@ -168,7 +168,7 @@ class VisualInputModel(SensoryModel):
             image_folder = 'images/shouval' if self.dataset=='Nature' \
                            else 'images/mcgill/foliage_a_combined' if self.dataset=='FoliageA' \
                            else 'images/mcgill/foliage_b_combined' if self.dataset=='FoliageB' \
-                           else self.data if len(self.data)>1 \
+                           else self.dataset if len(self.data)>1 \
                            else None
             return PatternCoordinatorImages(image_folder, **dict(params, **overrides))()
 
